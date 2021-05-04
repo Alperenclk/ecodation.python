24d256decb51e518572d2af971ab1164541a10df
Alperenclk <alperenclk1876@gmail.com>
2021-05-04 15:19:58 +0300

    aa

---
+++ 01_python_giris/001_print.py
@@ -0,0 +1,65 @@
+
+#region pirint
+
+"""
+print("alperen çelik")
+print("kocatepe üni.")
+
+print("alperen","<",3,"melike")
+"""
+
+"""
+escape karakteri
+
+print("alperen\nçelik\tmelike\ndilekçi")
+
+print("\\")
+print("\"")
+print('Merhaba Ece\'nin dünyasi')
+
+"""
+"""
+# end keywordu
+
+print("Hayatta"," En","Hakiki")
+print("Mürşit","İlimdir")
+print("-----------------") 
+
+print("Hayatta"," En","Hakiki",end="\n")
+print("Mürşit","İlimdir")
+print("-----------------")
+
+"""
+"""
+# sep keywordu              ***sep ve end default olarak örneklerdeki gibidir.
+
+print("Hayatta"," En","Hakiki","Mürşit","İlimdir")
+print("-----------------")
+
+print("Hayatta"," En","Hakiki","Mürşit","İlimdir",sep=" ")
+print("-----------------")
+"""
+"""
+#örnek 
+
+print("ecodation","egitim","kurumlari",sep=">>",end=".")
+"""
+#endregion
+
+#region örnek1
+
+print("Dunya'nin","en","guzel","sehri",sep="♥",end="→ISTANBUL")
+print("\n----------------\n")
+print("\"Ege\'nin\"",'"incisi"','"izmir"',sep="☼",end="!")
+print("\n----------------\n")
+#endregion
+
+#region type
+
+print(type("alperen"))
+print(type(1876))
+print(type(True))
+print(type(3.14))
+#endregion
+
+# bu bir testtir 0101
